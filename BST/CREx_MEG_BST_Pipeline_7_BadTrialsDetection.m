fb1b54d1556701c0a41456c22346953fc607585d
Valérie Chanoine <valerie.chanoine@blri.fr>
2016-10-11 16:06:54 +0200

    Update CREx_MEG_BST_Pipeline_7_BadTrialsDetection.m

--- BST/CREx_MEG_BST_Pipeline_7_BadTrialsDetection.m
+++ BST/CREx_MEG_BST_Pipeline_7_BadTrialsDetection.m
@@ -2,9 +2,9 @@ function BST_Pipeline_7_BadTrialsDetection
 
     % Example of script for Brainstorm software
     % Detection of Bad trials using peak-to-peak amplitude threshold
-    % Author: Valérie Chanoine, Research Engineer at Brain and Language
-    % Institute (http://www.blri.fr/)
-    % Partners: Jean-Michel Badier and Christian Bénar from 
+    % Author: ValÃ©rie Chanoine, Research Engineer at Brain and Language
+    % Research Institute (http://www.blri.fr/)
+    % Partners: Jean-Michel Badier and Christian BÃ©nar from 
     % MEG Center of Marseille (Timone Hospital,  France)
     % Date: April 12, 2015
 
