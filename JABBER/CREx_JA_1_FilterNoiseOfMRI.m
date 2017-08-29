379629ddccd7fcea01fa9d74377ab0429b62649b
Valérie Chanoine <valerie.chanoine@blri.fr>
2017-08-29 16:50:41 +0200

    Update CREx_JA_1_FilterNoiseOfMRI.m

--- JABBER/CREx_JA_1_FilterNoiseOfMRI.m
+++ JABBER/CREx_JA_1_FilterNoiseOfMRI.m
@@ -13,22 +13,19 @@ function CREx_JA_FilterNoiseOfMRI
     spm('defaults','fmri');  
     spm_jobman('initcfg');
     
-    %% Get anatomical MRI and make it isotropic (voxel size = 1mm x 1mm x 1mm)
+    % Get anatomical MRI and make it isotropic (voxel size = 1mm x 1mm x 1mm)
     anatDir = 'G:\JABBER\rawData\Patients\IRM\P01\';
     mri_original = fullfile(anatDir, 'P01.nii');
-    mri_resliced = fullfile(anatDir, 'P01_resliced.nii');
-    
+    mri_resliced = fullfile(anatDir, 'P01_resliced.nii');   
     reslice_nii(mri_original, mri_resliced);
-    %%
-    
-    %% Filtering for denoising using CAT12
+     
+    % Filtering for denoising using CAT12
     clear matlabbatch;
     matlabbatch{1}.spm.tools.cat.tools.sanlm.data = {mri_resliced};
     matlabbatch{1}.spm.tools.cat.tools.sanlm.prefix = 'sanlm_';
     matlabbatch{1}.spm.tools.cat.tools.sanlm.NCstr = -Inf;
     matlabbatch{1}.spm.tools.cat.tools.sanlm.rician = 0;
-    %%
-    
+   
     save(fullfile(anatDir, 'JA_SPM12_matlabbatch_T1Filtering.mat'),'matlabbatch'); 
     
     spm_jobman('initcfg');
